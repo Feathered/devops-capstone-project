2a6b30f91f02fe0f99106e0903986c67bd16e6dc
Feathered <p.sokolenkov@gmail.com>
2025-05-25 11:55:12 -0400

    dependencies test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -12,7 +12,7 @@ from tests.factories import AccountFactory
 from service.common import status  # HTTP Status Codes
 from service.models import db, Account, init_db
 from service.routes import app
-from flask import jsonify, request, abort
+from flask import Flask
 from service import talisman
 
 DATABASE_URI = os.getenv(
@@ -21,6 +21,7 @@ DATABASE_URI = os.getenv(
 
 BASE_URL = "/accounts"
 HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
+
 
 ######################################################################
 #  T E S T   C A S E S
